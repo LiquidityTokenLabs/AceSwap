f71ff8a038953037bf96f9aae1ed92c2f1e6a920
MIYABETaisei <mytspe46@gmail.com>
2022-10-14 21:54:03 +0900

    🧪 add comment #11

--- contracts/AceSwap.sol
+++ contracts/AceSwap.sol
@@ -1,15 +1,17 @@
 // SPDX-License-Identifier: None
 pragma solidity =0.8.16;
-import "hardhat/console.sol";
+import 'hardhat/console.sol';
 
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import '@openzeppelin/contracts/token/ERC721/IERC721.sol';
+import '@openzeppelin/contracts/utils/Counters.sol';
 
 contract AceSwap {
   using Counters for Counters.Counter;
   Counters.Counter private totalStake;
 
-//STORAGE
+  // test
+
+  //STORAGE
   //@param protocolFeeRatio: to calculate fee
   uint256 public protocolFeeRatio = 20 * 1000;
 
@@ -56,15 +58,35 @@ contract AceSwap {
   mapping(address => uint256) userLPoint;
 
   //@param eventId
-  uint256 public  eventId;
+  uint256 public eventId;
 
-//EVENT
-  event StakeNFT(address indexed staker, uint256[] tokenIds, uint256 LP, uint256 eventId);
-  event SwapNFTforFT(address indexed swapper, uint256[] tokenIds, uint256 totalFee, uint256 eventId);
-  event SwapFTforNFT(address indexed swapper, uint256[] tokenIds, uint256 totalFee, uint256 eventId);
-  event RemoveLP(address indexed staker, uint256[] tokenIds, uint256 removeLP, uint256 eventId);
+  //EVENT
+  event StakeNFT(
+    address indexed staker,
+    uint256[] tokenIds,
+    uint256 LP,
+    uint256 eventId
+  );
+  event SwapNFTforFT(
+    address indexed swapper,
+    uint256[] tokenIds,
+    uint256 totalFee,
+    uint256 eventId
+  );
+  event SwapFTforNFT(
+    address indexed swapper,
+    uint256[] tokenIds,
+    uint256 totalFee,
+    uint256 eventId
+  );
+  event RemoveLP(
+    address indexed staker,
+    uint256[] tokenIds,
+    uint256 removeLP,
+    uint256 eventId
+  );
 
-//CONSTRUCTOR
+  //CONSTRUCTOR
   //@notice initialization setting
   constructor(
     uint256 _spotPrice,
@@ -72,7 +94,7 @@ contract AceSwap {
     address _collectionAddress,
     address _protocolAddress,
     address _createAddress
-  ){
+  ) {
     spotPrice = _spotPrice;
     delta = _delta;
     collectionAddress = _collectionAddress;
@@ -87,8 +109,11 @@ contract AceSwap {
 
     //check
     unchecked {
-      for(uint256 i = 0; i < _tmpNum; i++){
-        require(IERC721(collectionAddress).ownerOf(tokenIds[i]) == msg.sender,"you are not owner");
+      for (uint256 i = 0; i < _tmpNum; i++) {
+        require(
+          IERC721(collectionAddress).ownerOf(tokenIds[i]) == msg.sender,
+          'you are not owner'
+        );
       }
     }
 
@@ -103,27 +128,38 @@ contract AceSwap {
 
     //intaraction
     unchecked {
-      for(uint256 i = 0; i < _tmpNum; i++){
-        IERC721(collectionAddress).safeTransferFrom(msg.sender,address(this),tokenIds[i],"");
+      for (uint256 i = 0; i < _tmpNum; i++) {
+        IERC721(collectionAddress).safeTransferFrom(
+          msg.sender,
+          address(this),
+          tokenIds[i],
+          ''
+        );
       }
     }
 
     eventId += 1;
     emit StakeNFT(msg.sender, tokenIds, _LP, eventId);
   }
-  
+
   //@notice swap FT for NFT
   function swapFTforNFT(uint256[] memory tokenIds) public payable {
     uint256 _tmpNum = tokenIds.length;
-    (uint256 _totalFee, uint256 _newSpotPrice) = _calcBuyInfo(_tmpNum, spotPrice, delta);
+    (uint256 _totalFee, uint256 _newSpotPrice) = _calcBuyInfo(
+      _tmpNum,
+      spotPrice,
+      delta
+    );
 
     //check
-    require(_tmpNum > 0, "Must ask for > 0 NFTs");
-    require(_tmpNum <= buyNum, "buy position not enough liquidity");
-    require(msg.value >= _totalFee, "not enough value");
+    require(_tmpNum > 0, 'Must ask for > 0 NFTs');
+    require(_tmpNum <= buyNum, 'buy position not enough liquidity');
+    require(msg.value >= _totalFee, 'not enough value');
     unchecked {
-      for(uint256 i = 0; i < _tmpNum; i++){
-        require(IERC721(collectionAddress).ownerOf(tokenIds[i]) == address(this));
+      for (uint256 i = 0; i < _tmpNum; i++) {
+        require(
+          IERC721(collectionAddress).ownerOf(tokenIds[i]) == address(this)
+        );
       }
     }
 
@@ -131,15 +167,20 @@ contract AceSwap {
     buyEventNum += _tmpNum;
     buyNum -= _tmpNum;
     sellNum += _tmpNum;
-    if(spotPrice != _newSpotPrice){
+    if (spotPrice != _newSpotPrice) {
       spotPrice = _newSpotPrice;
     }
 
     //intaraction
     payable(msg.sender).transfer(msg.value - _totalFee);
     unchecked {
-      for(uint256 i = 0; i < tokenIds.length; i++){
-        IERC721(collectionAddress).safeTransferFrom(address(this), msg.sender, tokenIds[i], "");
+      for (uint256 i = 0; i < tokenIds.length; i++) {
+        IERC721(collectionAddress).safeTransferFrom(
+          address(this),
+          msg.sender,
+          tokenIds[i],
+          ''
+        );
       }
     }
 
@@ -148,40 +189,54 @@ contract AceSwap {
   }
 
   //@notice swap NFT for FT
-  function swapNFTforFT(uint256[] memory tokenIds,uint256 minExpectFee) public payable {
+  function swapNFTforFT(uint256[] memory tokenIds, uint256 minExpectFee)
+    public
+    payable
+  {
     uint256 _tmpNum = tokenIds.length;
-    (uint256 _totalFee, uint256 _newSpotPrice) = _calcSellInfo(_tmpNum, spotPrice, delta);
+    (uint256 _totalFee, uint256 _newSpotPrice) = _calcSellInfo(
+      _tmpNum,
+      spotPrice,
+      delta
+    );
 
     //check
     unchecked {
-      for(uint256 i = 0; i < _tmpNum; i++){
+      for (uint256 i = 0; i < _tmpNum; i++) {
         require(IERC721(collectionAddress).ownerOf(tokenIds[i]) == msg.sender);
       }
     }
-    require(_totalFee >= minExpectFee, "The amount you get is lower than expected.");
-    require(address(this).balance >= _totalFee, "this contract is not enough");
-    require(_tmpNum <= sellNum, "sell position not enough liquidity");
-
+    require(
+      _totalFee >= minExpectFee,
+      'The amount you get is lower than expected.'
+    );
+    require(address(this).balance >= _totalFee, 'this contract is not enough');
+    require(_tmpNum <= sellNum, 'sell position not enough liquidity');
 
     //effect
     sellEventNum += _tmpNum;
     sellNum -= _tmpNum;
     buyNum += _tmpNum;
 
-    if(buyEventNum >= sellEventNum){
+    if (buyEventNum >= sellEventNum) {
       totalFee += sellEventNum * delta;
       buyEventNum -= sellEventNum;
       sellEventNum = 0;
     }
 
-    if(spotPrice != _newSpotPrice){
+    if (spotPrice != _newSpotPrice) {
       spotPrice = _newSpotPrice;
     }
 
     //intaraction
     unchecked {
-      for(uint256 i = 0; i < _tmpNum; i++){
-        IERC721(collectionAddress).safeTransferFrom(msg.sender, address(this), tokenIds[i], "");
+      for (uint256 i = 0; i < _tmpNum; i++) {
+        IERC721(collectionAddress).safeTransferFrom(
+          msg.sender,
+          address(this),
+          tokenIds[i],
+          ''
+        );
       }
     }
     payable(msg.sender).transfer(_totalFee);
@@ -198,12 +253,14 @@ contract AceSwap {
 
     //check
     unchecked {
-      for(uint256 i = 0; i < _tmpNum; i++){
-        require(IERC721(collectionAddress).ownerOf(tokenIds[i]) == address(this));
+      for (uint256 i = 0; i < _tmpNum; i++) {
+        require(
+          IERC721(collectionAddress).ownerOf(tokenIds[i]) == address(this)
+        );
       }
     }
-    require(userInitBuyNum[msg.sender] == _tmpNum,"Your NFT is remain yet");
-    require(buyNum >= _tmpNum,"Pool not enough NFT");
+    require(userInitBuyNum[msg.sender] == _tmpNum, 'Your NFT is remain yet');
+    require(buyNum >= _tmpNum, 'Pool not enough NFT');
 
     //effect
     buyNum -= userInitBuyNum[msg.sender];
@@ -214,12 +271,17 @@ contract AceSwap {
 
     //intaraction
     unchecked {
-      for(uint256 i = 0; i < _tmpNum; i++){
-        IERC721(collectionAddress).safeTransferFrom(address(this), msg.sender, tokenIds[i], "");
+      for (uint256 i = 0; i < _tmpNum; i++) {
+        IERC721(collectionAddress).safeTransferFrom(
+          address(this),
+          msg.sender,
+          tokenIds[i],
+          ''
+        );
       }
     }
 
-    if(_userFee > 0){
+    if (_userFee > 0) {
       payable(msg.sender).transfer(_userFee);
     }
 
@@ -235,12 +297,14 @@ contract AceSwap {
 
     //check
     unchecked {
-      for(uint256 i = 0; i < _tmpNum; i++){
-        require(IERC721(collectionAddress).ownerOf(tokenIds[i]) == address(this));
+      for (uint256 i = 0; i < _tmpNum; i++) {
+        require(
+          IERC721(collectionAddress).ownerOf(tokenIds[i]) == address(this)
+        );
       }
     }
-    require(userInitBuyNum[msg.sender] > buyNum,"Pool NFT is remain yet");
-    require(buyNum == tokenIds.length,"You would forgot NFT");
+    require(userInitBuyNum[msg.sender] > buyNum, 'Pool NFT is remain yet');
+    require(buyNum == tokenIds.length, 'You would forgot NFT');
 
     //effect
     buyNum -= tokenIds.length;
@@ -252,10 +316,15 @@ contract AceSwap {
 
     //intaraction
     payable(msg.sender).transfer(_userFee);
-    
+
     unchecked {
-      for(uint256 i = 0; i < _tmpNum; i++){
-        IERC721(collectionAddress).safeTransferFrom(address(this), msg.sender, tokenIds[i], "");
+      for (uint256 i = 0; i < _tmpNum; i++) {
+        IERC721(collectionAddress).safeTransferFrom(
+          address(this),
+          msg.sender,
+          tokenIds[i],
+          ''
+        );
       }
     }
 
@@ -264,107 +333,127 @@ contract AceSwap {
     emit RemoveLP(msg.sender, tokenIds, userLPoint[msg.sender], eventId);
   }
 
-//CALCULATION
+  //CALCULATION
   //@notice calculation LP
-  function _calcLP(uint256 _num)internal view returns(uint256){
+  function _calcLP(uint256 _num) internal view returns (uint256) {
     uint256 _LP;
 
-    if(totalLPoint == 0){
+    if (totalLPoint == 0) {
       _LP = _num * spotPrice;
-    }else{
-      _LP = totalLPoint * (_num * spotPrice) / (initBuyNum * spotPrice + totalFee);
+    } else {
+      _LP =
+        (totalLPoint * (_num * spotPrice)) /
+        (initBuyNum * spotPrice + totalFee);
     }
 
     return _LP;
   }
 
   //@notice calculation LP per Fee
-  function _calcRemoveLP(uint256 _num, address _user)internal view returns(uint256 _userFee){
-    uint256 _tmpLP = (initBuyNum * spotPrice + totalFee) * userLPoint[_user] / totalLPoint;
+  function _calcRemoveLP(uint256 _num, address _user)
+    internal
+    view
+    returns (uint256 _userFee)
+  {
+    uint256 _tmpLP = ((initBuyNum * spotPrice + totalFee) * userLPoint[_user]) /
+      totalLPoint;
     uint256 _tokenLP = spotPrice * _num;
-    if(_tmpLP > _tokenLP){
+    if (_tmpLP > _tokenLP) {
       _userFee = _tmpLP - _tokenLP;
-    }else{
+    } else {
       _userFee = 0;
     }
   }
 
   //@notice calculation total buy fee
-  function _calcBuyInfo (
-    uint256 _numItems, 
-    uint256 _spotPrice, 
+  function _calcBuyInfo(
+    uint256 _numItems,
+    uint256 _spotPrice,
     uint256 _delta
-  ) internal 
-    pure
-    returns(
-      uint256 totalFee,
-      uint256 newSpotPrice
-    ) {
-    
-    totalFee = _numItems * _spotPrice + (_numItems * (_numItems - 1) * _delta) / 2;
+  ) internal pure returns (uint256 totalFee, uint256 newSpotPrice) {
+    totalFee =
+      _numItems *
+      _spotPrice +
+      (_numItems * (_numItems - 1) * _delta) /
+      2;
 
     newSpotPrice = _spotPrice + _numItems * _delta;
   }
 
   //@notice calculation total sell fee
-  function _calcSellInfo (
-    uint256 _numItems, 
-    uint256 _spotPrice, 
+  function _calcSellInfo(
+    uint256 _numItems,
+    uint256 _spotPrice,
     uint256 _delta
-  ) internal 
-    pure
-    returns(
-      uint256 totalFee,
-      uint256 newSpotPrice
-    ) {
-    
-    totalFee = _numItems * (_spotPrice - _delta * 2  ) - (_numItems * (_numItems - 1) * _delta) / 2;
+  ) internal pure returns (uint256 totalFee, uint256 newSpotPrice) {
+    totalFee =
+      _numItems *
+      (_spotPrice - _delta * 2) -
+      (_numItems * (_numItems - 1) * _delta) /
+      2;
 
     newSpotPrice = _spotPrice - _numItems * _delta;
   }
 
-//GET
+  //GET
   //@notice get user per LP
-  function getUserLPoint(address user) external view returns(uint256) {
+  function getUserLPoint(address user) external view returns (uint256) {
     return userLPoint[user];
   }
 
   //@notice get user per buy num
-  function getUserInitBuyNum(address user) external view returns(uint256) {
+  function getUserInitBuyNum(address user) external view returns (uint256) {
     return userInitBuyNum[user];
   }
 
   //@notice get total buy price
-  function getCalcBuyInfo(uint256 _numItems)external view returns(uint256 totalFee){
-    totalFee = _numItems * spotPrice + (_numItems * (_numItems - 1) * delta) / 2;
+  function getCalcBuyInfo(uint256 _numItems)
+    external
+    view
+    returns (uint256 totalFee)
+  {
+    totalFee =
+      _numItems *
+      spotPrice +
+      (_numItems * (_numItems - 1) * delta) /
+      2;
     console.log(totalFee);
   }
 
   //@notice get total sell price
-  function getCalcSellInfo(uint256 _numItems)external view returns(uint256 totalFee){
-    totalFee = _numItems * (spotPrice - delta * 2  ) - (_numItems * (_numItems - 1) * delta) / 2;
+  function getCalcSellInfo(uint256 _numItems)
+    external
+    view
+    returns (uint256 totalFee)
+  {
+    totalFee =
+      _numItems *
+      (spotPrice - delta * 2) -
+      (_numItems * (_numItems - 1) * delta) /
+      2;
     console.log(totalFee);
   }
 
   //notice get user per Fee
-  function getUserFee(address _user)external view returns(uint256 _userFee){
-    uint256 _tmpLP = (initBuyNum * spotPrice + totalFee) * userLPoint[_user] / totalLPoint;
+  function getUserFee(address _user) external view returns (uint256 _userFee) {
+    uint256 _tmpLP = ((initBuyNum * spotPrice + totalFee) * userLPoint[_user]) /
+      totalLPoint;
     uint256 _tokenLP = spotPrice * userInitBuyNum[_user];
-    if(_tmpLP > _tokenLP){
+    if (_tmpLP > _tokenLP) {
       _userFee = _tmpLP - _tokenLP;
-    }else{
+    } else {
       _userFee = 0;
     }
   }
 
-//RECEIVED
+  //RECEIVED
   //@notice receive関数
   function onERC721Received(
-      address,
-      address,
-      uint256,
-      bytes memory
+    address,
+    address,
+    uint256,
+    bytes memory
   ) public virtual returns (bytes4) {
-      return this.onERC721Received.selector;
+    return this.onERC721Received.selector;
   }
 }
