8b7ef96854777a8d0e75d0129d5402b634ce9503
MIYABETaisei <mytspe46@gmail.com>
2022-10-19 21:54:06 +0900

    📦 solidity-coverage #14

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 import '@typechain/hardhat';
 import '@nomiclabs/hardhat-ethers';
+import 'solidity-coverage';
 
 const config: HardhatUserConfig = {
   solidity: '0.8.16',
